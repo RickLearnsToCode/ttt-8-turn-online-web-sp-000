9f6db3005418cbbddfa7128bd9b5ad427913238f
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-05 11:55:50 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+
+
+
+def display_board(b)
+  puts " #{b[0]} | #{b[1]} | #{b[2]} "
+  puts "-----------"
+  puts " #{b[3]} | #{b[4]} | #{b[5]} "
+  puts "-----------"
+  puts " #{b[6]} | #{b[7]} | #{b[8]} "
+end
+
+
+def position_taken?(board, index)
+  return !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+def valid_move?(board, index)
+  return index.between?(0,8) && !position_taken?
+end
+
+def input_to_index(board)
+  
